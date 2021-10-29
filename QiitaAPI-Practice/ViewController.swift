6aa60f1fa9805965fe6c438566ab4ff0160c27e8
Reon0429-cat <oonishireon110@gmail.com>
2021-10-30 01:53:56 +0900

    first commit

---
+++ QiitaAPI-Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QiitaAPI-Practice
+//
+//  Created by 大西玲音 on 2021/10/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
